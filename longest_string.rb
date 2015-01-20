1539fcee1b2a5fe78281fda34f77094f6d200e5f
Mark Goldenson <mjgold1@gmail.com>
2015-01-20 14:49:14 -0800

    Finished longest_string.rb

--- longest_string.rb
+++ longest_string.rb
@@ -5,6 +5,8 @@
 #
 # For example, longest_string(["a", "zzzz", "c"]) should return "zzzz" since
 # the other strings are 1 character long and "zzzz" is 4 characters long.
+# 
+# Ties go to the earliest word in the list.
 #
 # To get the length of a string in the variable str, call str.length
 # For example,
@@ -12,10 +14,22 @@
 #   str.length == 4
 
 def longest_string(list)
-  # This is your job. :)
+	longest = list.first
+	list.each do |current_str|
+		if current_str.length > longest.length
+			longest = current_str
+		end
+	end
+
+	longest
 end
 
 if __FILE__ == $0
-  # I'd advise putting some sanity checks here.
-  # How else will you be sure your code does what you think it does?
+  p longest_string(["a","zzzz","c"]) == "zzzz"
+  p longest_string(["hello","goodbye","hola"]) == "goodbye"
+  p longest_string(["a","b","c"]) == "a"
+  p longest_string(["a"]) == "a"
+  p longest_string(["hello"]) == "hello"
+  p longest_string(["supercalafragilicious","superawesome","super"]) == "supercalafragilicious"
+  p longest_string([""]) == ""
 end
